8f68ca01f9e7834197c2e92bb8e410db0e353a4b
hiteshsuthar14 <80826512+hiteshsuthar14@users.noreply.github.com>
2022-07-04 08:58:00 +0530

    Initial Commit

---
+++ My Notes App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Notes App
+//
+//  Created by Hitesh Suthar on 04/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Notes App/My_Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Notes_AppApp.swift
+//  My Notes App
+//
+//  Created by Hitesh Suthar on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
